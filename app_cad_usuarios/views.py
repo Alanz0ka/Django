9a394447c7f16f5e551d523840c91ef448eb611c
Alanz0ka <jaas4@aluno.ifal.edu.br>
2023-03-15 00:38:04 -0300

    Configurando as páginas e configurando a migração

--- app_cad_usuarios/views.py
+++ app_cad_usuarios/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'usuarios/home.html')
+
+def usuarios(requests):
+    pass
--- projeto_cad_usr/projeto_cad_usr/urls.py
+++ projeto_cad_usr/projeto_cad_usr/urls.py
@@ -4,4 +4,5 @@ from app_cad_usuarios import views
 urlpatterns = [
     #rota, view responsável, nome de referência
     path('',views.home,name='home'),
+    path('usuarios/',views.usuarios,name='listagem_usuarios'),
 ]
